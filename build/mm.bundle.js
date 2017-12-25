5a5ce9a67bc4cb5d52190289759c329080e4fed4
David Winner <kamsdvd@gmail.com>
2017-12-25 16:13:08 +0100

    Configuration in JS

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -4316,7 +4316,7 @@ angular.module('mm.core')
 }]);
 
 angular.module('mm.core')
-.constant('mmCoreLogEnabledDefault', true) 
+.constant('mmCoreLogEnabledDefault', false) 
 .constant('mmCoreLogEnabledConfigName', 'debug_enabled')
 .provider('$mmLog', ["mmCoreLogEnabledDefault", function(mmCoreLogEnabledDefault) {
     var isEnabled = mmCoreLogEnabledDefault,
@@ -7250,11 +7250,11 @@ angular.module('mm.core')
         };
         self.getDocsUrl = function(release, page) {
             page = page || 'Mobile_app';
-            var docsurl = 'https://docs.moodle.org/en/' + page;
+            var docsurl = 'http://support.darassa.com' + page;
             if (typeof release != 'undefined') {
                 var version = release.substr(0, 3).replace(".", "");
                 if (parseInt(version) >= 24) {
-                    docsurl = docsurl.replace('https://docs.moodle.org/', 'https://docs.moodle.org/' + version + '/');
+                    docsurl = docsurl.replace('http://support.darassa.com/', 'http://support.darassa.com/' + version + '/');
                 }
             }
             return $mmLang.getCurrentLanguage().then(function(lang) {
@@ -16502,7 +16502,7 @@ angular.module('mm.core.courses')
         };
         self.getController = function() {
             return function($scope) {
-                $scope.icon = 'ion-ionic';
+                $scope.icon = 'ion-ios-book';
                 $scope.title = 'mm.courses.mycourses';
                 $scope.state = 'site.mm_courses';
                 $scope.class = 'mm-mycourses-handler';
@@ -29063,7 +29063,7 @@ angular.module('mm.addons.calendar')
         };
         self.getController = function() {
             return function($scope) {
-                $scope.icon = 'ion-calendar';
+                $scope.icon = 'ion-android-calendar';
                 $scope.title = 'mma.calendar.calendarevents';
                 $scope.state = 'site.calendar';
                 $scope.class = 'mma-calendar-handler';
@@ -30450,7 +30450,7 @@ angular.module('mm.addons.files')
         };
         self.getController = function() {
             return function($scope) {
-                $scope.icon = 'ion-folder';
+                $scope.icon = 'ion-android-folder-open';
                 $scope.title = 'mma.files.files';
                 $scope.state = 'site.files';
                 $scope.class = 'mma-files-handler';
@@ -30753,7 +30753,7 @@ angular.module('mm.addons.frontpage')
         };
         self.getController = function() {
             return function($scope) {
-                $scope.icon = 'ion-home';
+                $scope.icon = 'ion-android-home';
                 $scope.title = 'mma.frontpage.sitehome';
                 $scope.state = 'site.frontpage';
                 $scope.class = 'mma-frontpage-handler';
@@ -30907,7 +30907,7 @@ angular.module('mm.addons.grades')
         };
         self.getController = function() {
             return function($scope, $state) {
-                $scope.icon = 'ion-stats-bars';
+                $scope.icon = 'ion-android-checkbox-outline';
                 $scope.title = 'mm.grades.grades';
                 $scope.class = 'mma-grades-mine-handler';
                 $scope.action = function($event, course) {
@@ -30944,7 +30944,7 @@ angular.module('mm.addons.grades')
             return function($scope) {
                 $scope.title = 'mm.grades.grades';
                 $scope.class = 'mma-grades-user-handler';
-                $scope.icon = 'ion-stats-bars';
+                $scope.icon = 'ion-android-checkbox-outline';
                 $scope.action = function($event) {
                     $event.preventDefault();
                     $event.stopPropagation();
@@ -31006,7 +31006,7 @@ angular.module('mm.addons.grades')
         };
         self.getController = function() {
             return function($scope) {
-                $scope.icon = 'ion-stats-bars';
+                $scope.icon = 'ion-android-done-all';
                 $scope.title = 'mm.grades.grades';
                 $scope.state = 'site.coursesgrades';
                 $scope.class = 'mma-grades-coursesgrades';
@@ -32053,7 +32053,7 @@ angular.module('mm.addons.messages')
                 var $mmPushNotificationsDelegate = $mmAddonManager.get('$mmPushNotificationsDelegate'),
                     $mmaPushNotifications = $mmAddonManager.get('$mmaPushNotifications'),
                     readChangedObserver, cronObserver;
-                $scope.icon = 'ion-chatbox';
+                $scope.icon = 'ion-chatbubbles';
                 $scope.title = 'mma.messages.messages';
                 $scope.state = 'site.messages';
                 $scope.class = 'mma-messages-handler';
@@ -33343,7 +33343,7 @@ angular.module('mm.addons.myoverview')
         };
         self.getController = function() {
             return function($scope) {
-                $scope.icon = 'ion-ionic';
+                $scope.icon = 'ion-ios-book';
                 $scope.title = 'mma.myoverview.pluginname';
                 $scope.state = 'site.myoverview';
                 $scope.class = 'mma-myoverview-handler';
@@ -34441,7 +34441,7 @@ angular.module('mm.addons.notifications')
                 var $mmPushNotificationsDelegate = $mmAddonManager.get('$mmPushNotificationsDelegate'),
                     $mmaPushNotifications = $mmAddonManager.get('$mmaPushNotifications'),
                     readChangedObserver, cronObserver;
-                $scope.icon = 'ion-ios-bell';
+                $scope.icon = 'ion-android-notifications-none';
                 $scope.title = 'mma.notifications.notifications';
                 $scope.state = 'site.notifications';
                 $scope.class = 'mma-notifications-handler';
@@ -71092,23 +71092,23 @@ angular.module('mm.addons.mod_workshop')
 angular.module('mm.core')
 
 .constant('mmCoreConfigConstants', {
-    "app_id" : "com.moodle.moodlemobile",
-    "appname": "Moodle Mobile",
-    "desktopappname": "Moodle Desktop",
+    "app_id" : "com.darassa.darassamobile",
+    "appname": "Darassa Mobile",
+    "desktopappname": "Darassa Desktop",
     "versioncode" : "2021",
     "versionname" : "3.4.0",
     "cache_expiration_time" : 300000,
-    "default_lang" : "en",
+    "default_lang" : "fr",
     "languages": {"ar": "عربي", "bg": "Български", "ca": "Català", "cs": "Čeština", "da": "Dansk", "de": "Deutsch", "de-du": "Deutsch - Du", "el": "Ελληνικά", "en": "English", "es": "Español", "es-mx": "Español - México", "eu": "Euskara", "fa": "فارسی", "fi": "Suomi", "fr" : "Français", "he" : "עברית", "hu": "magyar", "it": "Italiano", "ja": "日本語", "lt" : "Lietuvių", "mr": "मराठी", "nl": "Nederlands", "pl": "Polski", "pt-br": "Português - Brasil", "pt": "Português - Portugal", "ro": "Română", "ru": "Русский", "sr-cr": "Српски", "sr-lt": "Srpski", "sv": "Svenska", "tr" : "Türkçe", "uk" : "Українська", "zh-cn" : "简体中文", "zh-tw" : "正體中文"},
     "wsservice" : "moodle_mobile_app",
     "wsextservice" : "local_mobile",
-    "demo_sites": {"student": {"url": "https://school.demo.moodle.net", "username": "student", "password": "moodle"}, "teacher": {"url": "https://school.demo.moodle.net", "username": "teacher", "password": "moodle"}},
+    "demo_sites": {"student": {"url": "https://demo.darassa.com", "username": "etudiant", "password": "darassa"}, "instructeur": {"url": "https://demo.darassa.com", "username": "instructeur", "password": "darassa"}},
     "gcmpn": "694767596569",
     "customurlscheme": "moodlemobile",
-    "siteurl": "",
+    "siteurl": "http://192.168.1.105/darassa",
     "multisitesdisplay": "select",
     "skipssoconfirmation": "false",
     "forcedefaultlanguage": "false",
-    "privacypolicy": "https://moodle.org/mod/page/view.php?id=8148"
+    "privacypolicy": ""
 }
 );
